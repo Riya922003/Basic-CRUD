53acaec1b5a3c991dedf675eda8b8b624f19fdca
Riya922003 <riya98012@gmail.com>
2026-05-06 00:40:23 +0530

    feat: added service class

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,4 +1,42 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class EmployeeService {
+    private final EmployeeRepository employeeRepository ;
+    public EmployeeService (EmployeeRepository employeeRepository){
+        this.employeeRepository=employeeRepository ;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id){
+        return employeeRepository.findById(id).orElseThrow(() ->new RuntimeException("Employee not found with the id =" + id));
+    }
+
+    public Employee createEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Long id , Employee updatedEmployee){
+        Employee existing = getEmployeeById(id);
+        existing.setFirstName(updatedEmployee.getFirstName());
+        existing.setLastName(updatedEmployee.getLastName());
+        existing.setEmail(updatedEmployee.getEmail());
+        existing.setDepartment(updatedEmployee.getDepartment());
+
+        return employeeRepository.save(existing)
+    }
+
+    public void deleteEmplpyee (Long id){
+        employeeRepository.deleteById(id);
+    }
+
 }
+
