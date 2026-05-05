e2fbfc8b5c87f244677217ebe2b2cb646403df1b
Riya922003 <riya98012@gmail.com>
2026-05-06 00:17:18 +0530

    feat: added repository class

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -24,28 +24,28 @@ import jakarta.persistence.*;
         public String getFirstName (){
             return firstName;
         }
-        public void setfirstName(String firstName ){
+        public void setFirstName(String firstName ){
             this.firstName = firstName;
         }
 
         public String getLastName (){
             return lastName ;
         }
-        public void setlastName(String lastName){
+        public void setLastName(String lastName){
             this.lastName = lastName;
         }
 
         public String getEmail(){
             return email;
         }
-        public void setemail(String email){
+        public void setEmail(String email){
             this.email = email;
         }
 
         public String getDepartment (){
             return department;
         }
-        public void setdepartmanet(String department){
+        public void setDepartment(String department){
             this.department = department ;
         }
 
