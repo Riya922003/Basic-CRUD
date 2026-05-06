c75a20e0faf7d0b6632ca196b9f2c0b2919437b3
Riya922003 <riya98012@gmail.com>
2026-05-06 15:10:36 +0530

    final changes

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,13 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Employee;
 import com.example.demo.service.EmployeeService;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
 @RequestMapping("/api/employees")
+
 public class EmployeeController {
     private final EmployeeService employeeService ;
     public EmployeeController (EmployeeService employeeService){
@@ -21,17 +21,22 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        return ResponseEntity.ok(employeeService.getEmployeeById(id));
+    }
+
+    @PostMapping()
     public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
         return ResponseEntity.ok(employeeService.createEmployee(employee));
     }
 
-    @PutMapping
-    public ResponseEntity<EmployeeService> updateEmployeee(@PathVariable Long id , @RequestBody Employee employee){
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id , @RequestBody Employee employee){
         return ResponseEntity.ok(employeeService.updateEmployee(id , employee));
     }
 
-    @DeleteMapping("/{id")
-    public RequestEntity<Employee> deleteEmployee(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployee(id);
         return ResponseEntity.noContent().build();
     }
